a8679e38155f3b1eecb1a9b2c30c939919980b85
ChestonC <cchen208@ucsc.edu>
2021-05-03 17:26:42 -0700

    Update Header

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@
 Froggy Endless Runner
 By Cheston, Israel, Justin, and Tony
 Using Ethan Rafael's framerate decoupling
-Updated 4/24/2021
+Sound effects by www.fesliyanstudios.com
+5/3/2021
+In this game we used a mechanic of having a hunger meter and having the length of the game revolve around it.
+On the programming side, we implemented framerate decoupling to fix issues that occur for people with high refresh rate moniters.
+We used a combination of pixel art and digitally drawn art for the visuals and had a song made by one of us.
 */
 let config = {
     type: Phaser.AUTO,
